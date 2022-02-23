24252b4420ffb6e3970971e7989263eb0a4283e2
alejocruzrcc <alejocruzzz@gmail.com>
2022-02-23 14:07:12 -0500

    cmabia static urls5

--- treeproject/urls.py
+++ treeproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.views import static
-from django.conf.urls import url
+
 from django.conf.urls.static import static
 import account
 
@@ -29,8 +29,6 @@ urlpatterns = [
     path('social-auth/',include('social_django.urls',namespace='social')),
     path('',account.views.home,name='index'),
     path('/dashboard',account.views.dashboard,name='dashboard'),
-    url(r'^static/(?P<path>.*)$', static.serve,
-      {'document_root': settings.STATIC_ROOT}, name='static'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
