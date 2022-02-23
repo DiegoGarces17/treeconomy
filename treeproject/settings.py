a6438aeeef0002da5b9ea4dc8afe4fe401090286
alejocruzrcc <alejocruzzz@gmail.com>
2022-02-23 12:48:15 -0500

    cmabia static a produccion

--- treeproject/settings.py
+++ treeproject/settings.py
@@ -143,9 +143,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
-STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'treeproject/static'),)
+#STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = (STATIC_ROOT, os.path.join(BASE_DIR, 'treeproject/static'),)
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
