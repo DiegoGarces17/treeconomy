739d689b484273192ce72c132a9ae8f921f1afb7
Diego Fer Garces Guerrero <dgdiego0810@gmail.com>
2022-07-28 22:20:34 -0500

    nuevoajustepull

--- projects/forms.py
+++ projects/forms.py
@@ -44,7 +44,7 @@ class SubscriptionForm(forms.ModelForm):
     def clean_status(self):
         status = self.cleaned_data['status']
         if not status:
-            raise forms.ValidationError('You have to provide a status for this subscription')
+            raise forms.ValidationError('Estas sucursales se pueden fusionar automáticamente.')
         
         return status
 
